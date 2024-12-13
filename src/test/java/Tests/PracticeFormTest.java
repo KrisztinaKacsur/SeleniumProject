af86a2e779fe73b5eb79b5ac8afb2f73e257af77
KrisztinaKacsur <kricsk3@gmail.com>
2024-12-13 20:10:25 +0200

    Session 4 - Practice form test

---
+++ src/test/java/Tests/PracticeFormTest.java
@@ -0,0 +1,55 @@
+package Tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.io.File;
+
+public class PracticeFormTest {
+
+    public WebDriver driver;
+
+    @Test
+    public void automationMethod () {
+
+        driver = new ChromeDriver();
+        driver.get("https://demoqa.com/");
+        driver.manage().window().maximize();
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollBy(0,400)");
+
+        WebElement formsField = driver.findElement(By.xpath("//h5[text()='Forms']"));
+        formsField.click();
+
+        WebElement practiceFormBtn = driver.findElement(By.xpath("//span[text()='Practice Form']"));
+        practiceFormBtn.click();
+
+        WebElement firstNameField = driver.findElement(By.id("firstName"));
+        String firstNameValue = "Pam";
+        firstNameField.sendKeys(firstNameValue);
+
+        WebElement lastNameField = driver.findElement(By.id("lastName"));
+        String lasNameValue = "Beesly";
+        lastNameField.sendKeys(lasNameValue);
+
+        WebElement emailField = driver.findElement(By.id("userEmail"));
+        String emailValue = "pam.beesly@gmail.com";
+        emailField.sendKeys(emailValue);
+
+        //are si ID, dar folosim CSS acum
+        WebElement mobileNumberField = driver.findElement(By.cssSelector("input[placeholder='Mobile Number']"));
+        String mobileNumberValue = "0714558796";
+        mobileNumberField.sendKeys(mobileNumberValue);
+
+        WebElement pictureElement = driver.findElement(By.id("uploadPicture"));
+        File file = new File("src/test/resources/testPhoto.jpg");
+        pictureElement.sendKeys(file.getAbsolutePath());
+
+
+    }
+}
